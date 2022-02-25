7590177c774663824c1bd0bf89d410536efbd27b
koliadjun <koliadjun@gmail.com>
2022-02-25 16:42:43 +0200

    fix

--- server/app.js
+++ server/app.js
@@ -2,16 +2,20 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('../schema/schema.js');
 const mongoose = require('mongoose');
-const { connectToMongo } = require('./db/connection')
+const { connectToMongo } = require('./db/connection');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3005;
-require('dotenv').config()
+require('dotenv').config();
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true,
 }));
+
 app.listen(PORT, async (err) => {
     await connectToMongo()
     err ? console.log(error) : console.log('Server started!')
